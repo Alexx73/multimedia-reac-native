dae7c18bbcbdc8129635b222855a1e85006f5b85
Alexx73 <tristanluna@yahoo.com.ar>
2022-03-30 00:41:24 -0300

    auth started

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { Text, View, StyleSheet, Input, TouchableOpacity, TextInput } from 'react-native'
-
+import { Text, View, StyleSheet, Input, TouchableOpacity, TextInput, Button } from 'react-native'
+import Colors from '../global/Colors';
 
 function AuthScreen () {
 
@@ -11,21 +11,25 @@ function AuthScreen () {
     messageTarget = 'Login';
 
     return (
-        <View>
-            <Text> {title} </Text>
+        <View style={styles.screen} >
+            <View style={styles.container} >
+                <Text style={styles.title} > {title} </Text>
 
-            <Text> Email </Text>
-            <TextInput />
+                <Text  style={styles.label} > Email </Text>
+                <TextInput />
 
-            <Text> Clave </Text>
-            <TextInput />
+                <Text  style={styles.label}> Clave </Text>
+                <TextInput />
+                <Button title="REGISTRATSE"/>
 
 
-            <View>
-            <TouchableOpacity>
-                <Text> {messageAction} </Text>
-            </TouchableOpacity>
+                <View style={styles.prompt} >
+                <Text style={styles.promptMessage} > {message} </Text>
+                <TouchableOpacity>
+                    <Text style={styles.promptMessage} > {messageAction} </Text>
+                </TouchableOpacity>
 
+                </View>
             </View>
         </View>
     )
@@ -33,6 +37,38 @@ function AuthScreen () {
 
 
 const styles = StyleSheet.create({
+
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      title: {
+        fontSize: 24,
+        marginBottom: 18,
+        textAlign: 'center',
+      },
+      container: {
+        width: '80%',
+        maxWidth: 400,
+        padding: 12,
+        margin: 12,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: 'white',
+      },
+      prompt: {
+        alignItems: 'center',
+      },
+      promptMessage: {
+        fontSize: 16,
+        color: '#333',
+      },
+      promptButton: {
+        fontSize: 16,
+        color: Colors.primary,
+      },
     
 })
 
